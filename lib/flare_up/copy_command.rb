412538e49b29d6ea9de7dcba503cb3032a6b76bc
Robert Slifka <robert.slifka@gmail.com>
2014-08-08 21:19:09 -0700

    Support column specification

--- lib/flare_up/copy_command.rb
+++ lib/flare_up/copy_command.rb
@@ -6,20 +6,32 @@ module FlareUp
     attr_reader :data_source
     attr_reader :aws_access_key_id
     attr_reader :aws_secret_access_key
+    attr_reader :columns
 
     def initialize(table_name, data_source, aws_access_key_id, aws_secret_access_key)
       @table_name = table_name
       @data_source = data_source
       @aws_access_key_id = aws_access_key_id
       @aws_secret_access_key = aws_secret_access_key
+      @columns = []
     end
 
     def get_command
       # COPY table_name [ column_list ] FROM data_source CREDENTIALS access_credentials [options]
-      "COPY #{@table_name} FROM '#{@data_source}' CREDENTIALS '#{get_credentials}'"
+      "COPY #{@table_name} #{get_columns} FROM '#{@data_source}' CREDENTIALS '#{get_credentials}'"
+    end
+
+    def columns=(columns)
+      raise ArgumentError, 'Columns must be an array' unless columns.is_a?(Array)
+      @columns = columns
     end
 
     private
+
+    def get_columns
+      return '' if columns.empty?
+      "(#{@columns.join(', ').strip})"
+    end
 
     def get_credentials
       "aws_access_key_id=#{@aws_access_key_id};aws_secret_access_key=#{@aws_secret_access_key}"
--- spec/lib/flare_up/copy_command_spec.rb
+++ spec/lib/flare_up/copy_command_spec.rb
@@ -8,11 +8,40 @@ describe FlareUp::CopyCommand do
   its(:data_source) { should == 'TEST_DATA_SOURCE' }
   its(:aws_access_key_id) { should == 'TEST_ACCESS_KEY' }
   its(:aws_secret_access_key) { should == 'TEST_SECRET_KEY' }
+  its(:columns) { should == [] }
 
   describe '#get_command' do
     context 'when no optional fields are provided' do
       it 'should return a basic COPY command' do
-        expect(subject.get_command).to eq("COPY TEST_TABLE_NAME FROM 'TEST_DATA_SOURCE' CREDENTIALS 'aws_access_key_id=TEST_ACCESS_KEY;aws_secret_access_key=TEST_SECRET_KEY'")
+        expect(subject.get_command).to eq("COPY TEST_TABLE_NAME  FROM 'TEST_DATA_SOURCE' CREDENTIALS 'aws_access_key_id=TEST_ACCESS_KEY;aws_secret_access_key=TEST_SECRET_KEY'")
+      end
+    end
+
+    context 'when column names are provided' do
+      before do
+        subject.columns = %w(column_name1 column_name2)
+      end
+      it 'should include the column names in the command' do
+        expect(subject.get_command).to start_with('COPY TEST_TABLE_NAME (column_name1, column_name2) FROM')
+      end
+    end
+  end
+
+  describe '#columns=' do
+    context 'when an array' do
+      it 'should assign the property' do
+        subject.columns = %w(column_name1 column_name2)
+        expect(subject.columns).to eq(%w(column_name1 column_name2))
+      end
+    end
+
+    context 'when not an array' do
+      it 'should not assign the property and be an error' do
+        subject.columns = %w(column_name1)
+        expect {
+          subject.columns = '_'
+        }.to raise_error(ArgumentError)
+        expect(subject.columns).to eq(%w(column_name1))
       end
     end
   end
