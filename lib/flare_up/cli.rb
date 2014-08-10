062e1a93a389e5fa3f1c2621907edc534a826140
Robert Slifka <robert.slifka@gmail.com>
2014-08-10 16:36:23 -0700

    Option progress

--- lib/flare_up/cli.rb
+++ lib/flare_up/cli.rb
@@ -2,6 +2,31 @@ module FlareUp
 
   class CLI < Thor
 
+    desc 'copy DATA_SOURCE REDSHIFT_ENDPOINT DATABASE TABLE', 'COPY data into REDSHIFT_ENDPOINT from DATA_SOURCE into DATABASE.TABLE'
+    long_desc = <<-LONGDESC
+    `flare-up copy` executes the Redshift COPY command, loading data from
+    DATA_SOURCE into DATABASE_NAME.TABLE_NAME at REDSHIFT_ENDPOINT.
+
+    TODO Examples:
+    > $ flare-up copy
+    LONGDESC
+
+    option :column_list, :type => :array
+    option :copy_options, :type => :string, :desc => "Appended to the end of the COPY command; enclose \"IN QUOTES\""
+    option :aws_access_key, :type => :string, :desc => "Required unless ENV['AWS_ACCESS_KEY_ID'] is set."
+    option :aws_secret_key, :type => :string, :desc => "Required unless ENV['AWS_SECRET_ACCESS_KEY'] is set."
+
+    def copy(data_source, endpoint, database_name, table_name)
+      boot_options = {
+        :data_source => data_source,
+        :redshift_endpoint => endpoint,
+        :database => database_name,
+        :table => table_name
+      }
+      options.each { |k, v| boot_options[k.to_sym] = v }
+      Boot.boot(boot_options)
+    end
+
   end
 
 end
--- spec/lib/flare_up/cli_spec.rb
+++ spec/lib/flare_up/cli_spec.rb
@@ -1,9 +1,76 @@
 describe FlareUp::CLI do
 
-  describe '.class' do
-    it 'should be a Thor descendant so it can boot' do
-      expect(FlareUp::CLI.new).to be_a(Thor)
+  let(:required_arguments) { %w(copy TEST_DATA_SOURCE TEST_REDSHIFT_ENDPOINT TEST_DATABASE TEST_TABLE) }
+  let(:required_options) do
+    {
+      :data_source => 'TEST_DATA_SOURCE',
+      :redshift_endpoint => 'TEST_REDSHIFT_ENDPOINT',
+      :database => 'TEST_DATABASE',
+      :table => 'TEST_TABLE'
+    }
+  end
+
+  describe '#copy' do
+
+    context 'when no options are specified' do
+      it 'should boot with the proper options' do
+        expect(FlareUp::Boot).to receive(:boot).with(required_options)
+        FlareUp::CLI.start(required_arguments)
+      end
     end
+
+    context 'when column ordering is specified' do
+      it 'should boot with the proper options' do
+        expect(FlareUp::Boot).to receive(:boot).with(required_options.merge(:column_list => %w(c1 c2 c3)))
+        FlareUp::CLI.start(required_arguments + %w(--column_list c1 c2 c3))
+      end
+    end
+
+    context 'when COPY options are specified' do
+      it 'should boot with the proper options' do
+        expect(FlareUp::Boot).to receive(:boot).with(required_options.merge(:copy_options => 'TEST_COPY_OPTIONS WITH A SPACE'))
+        FlareUp::CLI.start(required_arguments + ['--copy_options', 'TEST_COPY_OPTIONS WITH A SPACE'])
+      end
+    end
+
+    describe 'AWS credentials' do
+
+      describe 'access key' do
+        context 'when an AWS access key is specified' do
+          it 'should boot with the proper options' do
+            expect(FlareUp::Boot).to receive(:boot).with(required_options.merge(:aws_access_key => 'TEST_AWS_ACCESS_KEY'))
+            FlareUp::CLI.start(required_arguments + %w(--aws_access_key TEST_AWS_ACCESS_KEY))
+          end
+        end
+        context 'when an AWS access key is not specified' do
+          context 'when the key is available via ENV' do
+            it 'should boot with the key from the environment'
+          end
+          context 'when the key is not available via ENV' do
+            it 'should be an error'
+          end
+        end
+      end
+
+      describe 'secret key' do
+        context 'when an AWS secret key is specified' do
+          it 'should boot with the proper options' do
+            expect(FlareUp::Boot).to receive(:boot).with(required_options.merge(:aws_secret_key => 'TEST_AWS_SECRET_KEY'))
+            FlareUp::CLI.start(required_arguments + %w(--aws_secret_key TEST_AWS_SECRET_KEY))
+          end
+        end
+        context 'when an AWS secret key is not specified' do
+          context 'when the key is available via ENV' do
+            it 'should boot with the key from the environment'
+          end
+          context 'when the key is not available via ENV' do
+            it 'should be an error'
+          end
+        end
+      end
+
+    end
+
   end
 
 end
