c6b6bd2b01a38cb53ed7131d1d79f14b12fb1de1
Robert Slifka <robert.slifka@gmail.com>
2014-08-10 20:11:40 -0700

    Extraction for testing; control flow remains

--- lib/flare_up/boot.rb
+++ lib/flare_up/boot.rb
@@ -2,8 +2,40 @@ module FlareUp
 
   class Boot
 
+    # TODO: This control flow is untested
     def self.boot(options)
+      conn = create_connection(options)
+      copy = create_copy_command(options)
 
+      errors = copy.execute(conn)
+      if errors.empty?
+        return
+      end
+
+      errors.each do |e|
+        puts e.pretty_print
+      end
+    end
+
+    def self.create_connection(options)
+      FlareUp::Connection.new(
+        options[:redshift_endpoint],
+        options[:database],
+        options[:redshift_username],
+        options[:redshift_password]
+      )
+    end
+
+    def self.create_copy_command(options)
+      copy = FlareUp::CopyCommand.new(
+        options[:table],
+        options[:data_source],
+        options[:aws_access_key],
+        options[:aws_secret_key]
+      )
+      copy.columns = options[:column_list] if options[:column_list]
+      copy.options = options[:copy_options] if options[:copy_options]
+      copy
     end
 
   end
--- spec/lib/flare_up/boot_spec.rb
+++ spec/lib/flare_up/boot_spec.rb
@@ -6,4 +6,64 @@ describe FlareUp::Boot do
     end
   end
 
+  describe '.create_connection' do
+    let(:options) {
+      {
+        :redshift_endpoint => 'TEST_REDSHIFT_ENDPOINT',
+        :database => 'TEST_DATABASE',
+        :redshift_username => 'TEST_REDSHIFT_USERNAME',
+        :redshift_password => 'TEST_REDSHIFT_PASSWORD'
+      }
+    }
+
+    it 'should create a connection' do
+      connection = FlareUp::Boot.create_connection(options)
+      expect(connection.host).to eq('TEST_REDSHIFT_ENDPOINT')
+      expect(connection.dbname).to eq('TEST_DATABASE')
+      expect(connection.user).to eq('TEST_REDSHIFT_USERNAME')
+      expect(connection.password).to eq('TEST_REDSHIFT_PASSWORD')
+    end
+  end
+
+  describe '.create_copy_command' do
+
+    let(:options) {
+      {
+        :table          => 'TEST_TABLE',
+        :data_source    => 'TEST_DATA_SOURCE',
+        :aws_access_key => 'TEST_ACCESS_KEY',
+        :aws_secret_key => 'TEST_SECRET_KEY'
+      }
+    }
+
+    it 'should create a proper copy command' do
+      command = FlareUp::Boot.create_copy_command(options)
+      expect(command.table_name).to eq('TEST_TABLE')
+      expect(command.data_source).to eq('TEST_DATA_SOURCE')
+      expect(command.aws_access_key_id).to eq('TEST_ACCESS_KEY')
+      expect(command.aws_secret_access_key).to eq('TEST_SECRET_KEY')
+    end
+
+    context 'when columns are specified' do
+      before do
+        options.merge!(:column_list => ['c1'])
+      end
+      it 'should create a proper copy command' do
+        command = FlareUp::Boot.create_copy_command(options)
+        expect(command.columns).to eq(['c1'])
+      end
+    end
+
+    context 'when options are specified' do
+      before do
+        options.merge!(:copy_options => '_')
+      end
+      it 'should create a proper copy command' do
+        command = FlareUp::Boot.create_copy_command(options)
+        expect(command.options).to eq('_')
+      end
+    end
+
+  end
+
 end
