be6aebc7cf1484d8da95b99987302ee53e376478
Robert Slifka <robert.slifka@gmail.com>
2014-08-10 22:09:48 -0700

    Sample commands for various outcomes

--- resources/load_hearthstone_cards.rb
+++ resources/load_hearthstone_cards.rb
@@ -8,7 +8,57 @@ data_source = 's3://slif-redshift/hearthstone_cards_short_list.csv'
 conn = FlareUp::Connection.new(host_name, db_name, ENV['REDSHIFT_USERNAME'], ENV['REDSHIFT_PASSWORD'])
 
 copy = FlareUp::CopyCommand.new(table_name, data_source, ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-copy.columns = ['name', 'cost', 'attack', 'health', 'description']
+copy.columns = %w(name cost attack health description)
 copy.options = "REGION 'us-east-1' CSV"
 
 result = copy.execute(conn)
+
+good_command = <<-COMMAND
+flare-up copy \
+  s3://slif-redshift/hearthstone_cards_short_list.csv \
+  flare-up-test.cskjnp4xvaje.us-west-2.redshift.amazonaws.com \
+  dev \
+  hearthstone_cards \
+  --column-list name cost attack health description \
+  --copy_options "REGION 'us-east-1' CSV"
+COMMAND
+
+bad_data_source_command = <<-COMMAND
+flare-up copy \
+  s3://slif-redshift/hearthstone_cards_short_lissdsdsdsdsdsdsd.csv \
+  flare-up-test.cskjnp4xvaje.us-west-2.redshift.amazonaws.com \
+  dev \
+  hearthstone_cards \
+  --column-list name cost attack health description \
+  --copy_options "REGION 'us-east-1' CSV"
+COMMAND
+
+bad_data_command = <<-COMMAND
+flare-up copy \
+  s3://slif-redshift/hearthstone_cards_broken.csv \
+  flare-up-test.cskjnp4xvaje.us-west-2.redshift.amazonaws.com \
+  dev \
+  hearthstone_cards \
+  --column-list name cost attack health description \
+  --copy_options "REGION 'us-east-1' CSV"
+COMMAND
+
+other_zone_bucket = <<-COMMAND
+flare-up copy \
+  s3://slif-redshift/hearthstone_cards_short_list.csv \
+  flare-up-test.cskjnp4xvaje.us-west-2.redshift.amazonaws.com \
+  dev \
+  hearthstone_cards \
+  --column-list name cost attack health description \
+  --copy_options CSV
+COMMAND
+
+busted_options = <<-COMMAND
+flare-up copy \
+  s3://slif-redshift/hearthstone_cards_short_list.csv \
+  flare-up-test.cskjnp4xvaje.us-west-2.redshift.amazonaws.com \
+  dev \
+  hearthstone_cards \
+  --column-list name cost attack health description \
+  --copy_options "CSV ;lmlkmlk3"
+COMMAND
